e458a804dff94f1612d2a8025ed8c5fc2d7908a0
Abakaxato <cauakika@gmail.com>
2025-02-19 20:58:59 -0300

    ⬆️ Adicionando novas classes Funcionario e Professor ao pacote de herança⬆️

---
+++ Heranca/src/Funcionario.java
@@ -0,0 +1,35 @@
+public class Funcionario extends Pessoa{
+
+    //Atributos
+
+    private String setor;
+    private boolean trabalhando = false;
+
+    // Métodos
+    public void mudartraba(String s){
+        if (trabalhando){
+            this.setSetor(s);
+        }else {
+            this.setTrabalhando(true);
+            System.out.println("Funcionario não estava trabalhando, adicionando trabalho...");
+        }
+    }
+
+    // Getters and Setters
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public boolean isTrabalhando() {
+        return trabalhando;
+    }
+
+    public void setTrabalhando(boolean trabalhando) {
+        this.trabalhando = trabalhando;
+    }
+}
