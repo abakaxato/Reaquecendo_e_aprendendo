370a25ffda26595cc38d9e60e32022fa4f236292
Abakaxato <cauakika@gmail.com>
2025-02-19 20:42:41 -0300

    ✨ Introduzindo primeiras classes de estudo sobre Herança ✨

---
+++ Heranca/src/Aluno.java
@@ -0,0 +1,34 @@
+public class Aluno {
+
+    //Atributos
+
+    private int matricula;
+    private String curso;
+
+    // Métodos
+
+    public void cancelarMatri(){
+        this.matricula = 0;
+        this.setCurso("Curso Cancelado ou inexistente");
+
+    }
+
+
+    //Getters and Setters
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
---
+++ Heranca/src/Pessoa.java
@@ -0,0 +1,39 @@
+public class Pessoa {
+
+    //Atributos
+
+    private String nome,sexo;
+    private int idade;
+
+    // Métodos
+
+    public void fazerAni (){
+        this.setIdade(+1);
+    }
+
+    //Getters and Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
