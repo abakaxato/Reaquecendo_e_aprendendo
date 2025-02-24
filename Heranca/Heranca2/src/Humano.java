82d5e40d6f8e6dc4f879908d6704595749917283
Abakaxato <cauakika@gmail.com>
2025-02-24 20:27:42 -0300

    🚚Movendo Classes "Tecnico" e "Bolsista" para o modulo "Herança 2" 🚚

---
+++ Heranca/Heranca2/src/Humano.java
@@ -0,0 +1,41 @@
+public abstract class Humano {
+
+    //Atributos
+
+    private String nome,sexo;
+    private int idade;
+
+    //Métodos
+    public final void fazerAniv(){
+        this.setIdade(+1);
+
+    }
+
+
+
+    // Getters and Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
