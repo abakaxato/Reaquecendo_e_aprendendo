3d5aa8c32747d1cc07ff1cd005618572c32d6df2
Abakaxato <cauakika@gmail.com>
2025-02-25 21:37:44 -0300

    🎨Atualiando a classe "Mamiferos" e inserindo seu corpo🎨

---
+++ Heranca/Heranca2/Polimorfismo/src/Mamiferos.java
@@ -0,0 +1,30 @@
+public class Mamiferos extends Animal{
+
+    //Atributos
+    private String corPelo;
+
+    // Getters and setters
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+
+    //Métodos da Super Classe (Obrigatórios e obrigatorios sobrepor)
+    @Override
+    public void locomover() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+
+    @Override
+    public String emitirSom() {
+        return " au au ";
+    }
+}
