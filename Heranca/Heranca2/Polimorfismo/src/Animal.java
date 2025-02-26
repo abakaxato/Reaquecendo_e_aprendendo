4f98b5fc00a32c6ac705dcaac68e85756fe88458
Abakaxato <cauakika@gmail.com>
2025-02-25 21:24:14 -0300

    ✨Inserindo o modulo "Polimorfismo" com a classe "Animal"✨

---
+++ Heranca/Heranca2/Polimorfismo/src/Animal.java
@@ -0,0 +1,13 @@
+public abstract class Animal {
+
+    //Atributos
+    protected int idade, menbros;
+    protected float peso;
+
+    //Métodos
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract String emitirSom();
+
+
+}
