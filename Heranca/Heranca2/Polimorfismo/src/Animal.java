d02efb3969029071e8cd56313561514eff5d702d
Abakaxato <cauakika@gmail.com>
2025-03-12 21:40:40 -0300

    ✨Inserindo o construtor da Superclasse Animal✨

--- Heranca/Heranca2/Polimorfismo/src/Animal.java
+++ Heranca/Heranca2/Polimorfismo/src/Animal.java
@@ -1,7 +1,7 @@
 public abstract class Animal {
 
     //Atributos
-    protected int idade, menbros;
+    protected int idade, membros;
     protected float peso;
 
     //Métodos
@@ -9,5 +9,27 @@ public abstract class Animal {
     public abstract void alimentar();
     public abstract String emitirSom();
 
+    public int getIdade() {
+        return idade;
+    }
 
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMembros() {
+        return membros;
+    }
+
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
 }
