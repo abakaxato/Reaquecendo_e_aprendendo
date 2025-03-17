d999ea77b6169d94fc5825ff5f3dd32afdcdfad0
Abakaxato <cauakika@gmail.com>
2025-03-17 19:11:28 -0300

    🚀 Criando a classe LOBO para estudo de polimorfismo(Sobrecarga) 🚀

---
+++ Heranca/Heranca2/Polimorfismo/src/Lobo.java
@@ -0,0 +1,24 @@
+public class Lobo extends Mamiferos{
+
+
+    public Lobo(int idade, int membros, float peso, String corPelo) {
+        super(idade, membros, peso, corPelo);
+    }
+
+    @Override
+    public void locomover() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+
+    @Override
+    public String emitirSom() {
+        return "auuuuuuuu";
+    }
+
+
+}
