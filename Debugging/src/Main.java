536eb2956c1233ad33a29c6ebcebd9549327619a
Abakaxato <cauakika@gmail.com>
2025-04-01 17:01:24 -0300

    ✨Estudando Debugging✨

---
+++ Debugging/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+    int a = 2;
+    int b = 9;
+    int c = 11;
+
+    int aTimesB = a*b;
+    if ( aTimesB > c){
+        System.out.println("Bigger");
+    }else{
+        System.out.println("Smaller");
+    }
+
+
+    }
+}
